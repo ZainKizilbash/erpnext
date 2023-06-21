761f0bdb45248705ec5deb1f3459b34cd560596a
AUHS2001 <ateebshaikh2001@gmail.com>
2023-06-21 16:31:33 +0500

    Feat(Opportunity): set sales person details in opportunity

--- erpnext/crm/doctype/opportunity/opportunity.py
+++ erpnext/crm/doctype/opportunity/opportunity.py
@@ -109,6 +109,7 @@ class Opportunity(TransactionBase):
 		self.set_customer_details()
 		self.set_item_details()
 		self.set_applies_to_details()
+		self.set_sales_person_details()
 
 	def set_customer_details(self):
 		customer_details = get_customer_details(self.as_dict())
@@ -136,6 +137,23 @@ class Opportunity(TransactionBase):
 		for k, v in applies_to_details.items():
 			if self.meta.has_field(k) and not self.get(k) or k in force_applies_to_fields:
 				self.set(k, v)
+
+	def set_sales_person_details(self):
+		if not self.sales_person:
+			return
+
+		employee_id = frappe.db.get_value("Sales Person", self.sales_person, ['employee'])
+
+		if not employee_id:
+			return
+
+		employee = frappe.db.get_value("Employee", employee_id, ['cell_number', 'personal_email'], as_dict=1)
+
+		if not employee:
+			return
+
+		self.sales_person_contact_no = employee.cell_number
+		self.sales_person_email = employee.personal_email
 
 	def validate_financer(self):
 		if self.get('financer'):
---
+++ erpnext/patches/v14_0/set_sales_person_details_in_opportunity.py
@@ -0,0 +1,14 @@
+import frappe
+
+def execute():
+    frappe.reload_doc("crm", "doctype", "opportunity")
+
+    frappe.db.sql("""
+        UPDATE `tabOpportunity` AS opp
+        INNER JOIN `tabSales Person` AS sp 
+            ON sp.name = opp.sales_person
+        INNER JOIN `tabEmployee` AS emp 
+            ON emp.name = sp.employee
+        SET opp.sales_person_contact_no = emp.cell_number, 
+            opp.sales_person_email = emp.personal_email
+    """)
