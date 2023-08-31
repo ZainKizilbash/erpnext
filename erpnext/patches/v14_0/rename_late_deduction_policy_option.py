3ecae686e16013c39ac098de64d60f7ecd58cd52
Saif Ur Rehman <saif@paralogic.io>
2023-09-01 01:00:23 +0500

    fix: patch reload_doctype

--- erpnext/patches/v14_0/rename_late_deduction_policy_option.py
+++ erpnext/patches/v14_0/rename_late_deduction_policy_option.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doctype("Leave Policy")
 	frappe.db.sql("""
 		update `tabLeave Policy`
 		set late_deduction_policy = 'No of Late Days as Leave Without Pay'
