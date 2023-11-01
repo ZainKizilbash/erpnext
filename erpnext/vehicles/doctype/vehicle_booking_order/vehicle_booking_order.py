2d6bf32accc3afa766fbf41c1768bf85634d8d27
murtazaabidi1 <murtazaabidi93@gmail.com>
2023-11-01 18:19:55 +0500

    style: fix spaces

--- erpnext/vehicles/doctype/vehicle_booking_order/vehicle_booking_order.py
+++ erpnext/vehicles/doctype/vehicle_booking_order/vehicle_booking_order.py
@@ -996,7 +996,7 @@ def get_vbos_for_sales_invoice(from_date, to_date, item_code=None):
             AND vbo.vehicle_delivered_date <= %(to_date)s
             AND vbo.docstatus = 1
             {item_condition}
-        """.format(item_condition = item_condition), {'item_code': item_code, 'from_date': from_date, 'to_date': to_date}, as_dict =1)
+        """.format(item_condition=item_condition), {'item_code': item_code, 'from_date': from_date, 'to_date': to_date}, as_dict=1)
 
     if not vbo_names:
         frappe.throw(_("No Vehicle Booking Orders found"))
