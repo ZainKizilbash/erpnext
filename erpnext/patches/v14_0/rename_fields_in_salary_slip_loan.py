dc69e3e54a81e6fab131e142eaa18ad7a5ca8662
murtazaabidi1 <murtazaabidi93@gmail.com>
2023-11-16 14:46:29 +0500

    chore: PR requested changes

--- erpnext/patches/v14_0/rename_fields_in_salary_slip_loan.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-import frappe
-from frappe.model.utils.rename_field import rename_field
-
-def execute():
-	frappe.reload_doc("HR", "doctype", "advances")
-	rename_field("Salary Slip Loan", "total_payment", "repayment_amount")
-	rename_field("Salary Slip Loan", "total_balance_amount", "balance_amount")
--- erpnext/patches/v14_0/set_salary_slip_loan_fields.py
+++ erpnext/patches/v14_0/set_salary_slip_loan_fields.py
@@ -1,7 +1,10 @@
+from __future__ import unicode_literals
 import frappe
+from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	frappe.reload_doc("HR", "doctype", "salary_slip_loan")
+	rename_field("Salary Slip Loan", "total_payment", "repayment_amount")
 
 	frappe.db.sql("""
 		UPDATE `tabSalary Slip Loan` cssl
@@ -11,5 +14,5 @@ def execute():
 			cssl.disbursement_date = loan.disbursement_date,
 			cssl.total_loan_amount = loan.total_payment,
 			cssl.pending_loan_amount = cssl.total_payment + rps.balance_loan_amount
-			cssl.total_balance_amount = rps.balance_loan_amount
+			cssl.balance_amount = rps.balance_loan_amount
 	""")
