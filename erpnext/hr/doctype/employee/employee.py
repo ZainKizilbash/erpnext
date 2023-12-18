54910db3d2703e19da812766e82c2d70a9760db5
murtazaabidi1 <murtazaabidi93@gmail.com>
2023-12-18 17:34:05 +0500

    fix: unhashable type 'list'

--- erpnext/hr/doctype/employee/employee.py
+++ erpnext/hr/doctype/employee/employee.py
@@ -299,7 +299,8 @@ def get_holiday_list_for_employee(employee, raise_exception=True):
 	from erpnext.hr.doctype.holiday_list.holiday_list import get_default_holiday_list
 
 	if employee:
-		holiday_list, company = frappe.db.get_value("Employee", employee, ["holiday_list", "company"], cache=1)
+		holiday_list = frappe.db.get_value("Employee", employee, "holiday_list", cache=1)
+		company = frappe.db.get_value("Employee", employee, "company", cache=1)
 	else:
 		holiday_list = ''
 		company = frappe.db.get_single_value("Global Defaults", "default_company")
